557d20e88187af5cecde62462813f5c6c55272d0
Tony Hu <tony990309@gmail.com>
2025-08-19 21:26:14 +0800

    refactor: remove unique constraint in users name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-
   has_many :clock_ins, dependent: :destroy
   has_many :sleep_durations, dependent: :destroy
   has_one :sleeping_duration, -> { sleeping }, class_name: 'SleepDuration'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_16_084222) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_19_125823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_16_084222) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name"
   end
 
   add_foreign_key "clock_ins", "users"
