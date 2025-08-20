e11f9807482051abe0939400a388c275fea89e0d
Tony Hu <tony990309@gmail.com>
2025-08-20 23:05:14 +0800

    refactor clock in service

--- app/services/clock_ins/creator.rb
+++ app/services/clock_ins/creator.rb
@@ -13,22 +13,25 @@ class ClockIns::Creator < ServiceCaller
   private
 
   def check_action
-    raise ServiceError.new(:user_is_sleeping) if @user.sleeping? && @action_type == 'sleep'
-    raise ServiceError.new(:user_is_awake) if @user.awake? && @action_type == 'wake'
+    @existing_sleep_duration = SleepDuration.find_by(user_id: @user.id, end_time: nil)
+    raise ServiceError.new(:user_is_sleeping) if @existing_sleep_duration.present? && @action_type == 'sleep'
+    raise ServiceError.new(:user_is_awake) if @existing_sleep_duration.nil? && @action_type == 'wake'
   end
 
   def clock_in
     ActiveRecord::Base.transaction do
-      clock_in_record = ClockIn.create_from_request(@user.id, @action_type)
+      clock_in_record = ClockIn.create!(user_id: @user.id, action_type: @action_type)
       clock_in_time = clock_in_record.created_at
-      sleeping_duration = SleepDuration.setup_for(@user)
+      sleeping_duration = @existing_sleep_duration || SleepDuration.new(user_id: @user.id)
 
       case @action_type
       when 'sleep'
-        sleeping_duration.update(start_time: clock_in_time)
+        sleeping_duration.start_time = clock_in_time
       when 'wake'
-        sleeping_duration.update(end_time: clock_in_time)
+        sleeping_duration.end_time = clock_in_time
       end
+
+      sleeping_duration.save!
     end
   end
 
