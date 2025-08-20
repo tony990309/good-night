3ffe74ffdbe344b87cae16262e891211194de149
Tony Hu <tony990309@gmail.com>
2025-08-20 21:07:55 +0800

    refactor unfollow_spec

--- spec/controllers/api/v1/users/follow_controller/unfollow_spec.rb
+++ spec/controllers/api/v1/users/follow_controller/unfollow_spec.rb
@@ -8,29 +8,20 @@ RSpec.describe Api::V1::Users::FollowController, type: :request do
     example.metadata[:rpdoc_action_name] = '取消追蹤使用者'
     example.metadata[:rpdoc_example_folders] = ['v1', 'users', 'follow']
 
-    @self_user = FactoryBot.create(:user)
-    @other_user = FactoryBot.create(:user)
-    FactoryBot.create(:follow, follower: @self_user, following: @other_user)
+    @user = FactoryBot.create(:two_following_user)
 
     @headers = { 'Content-Type' => 'application/json' }
     @path = "/api/v1/users/unfollow"
-    @params = { user_id: @self_user.id }
+    @params = { user_id: @user.id }
   end
 
   describe '#create' do
     it 'should return 200 and unfollow following user', rpdoc_example_key: 200, rpdoc_example_name: 'unfollow user success' do
-      @params[:following_id] = @other_user.id
+      following_user = @user.following.first
+      @params[:following_id] = following_user.id
       post @path, params: @params.to_json, headers: @headers
       expect(response).to have_http_status(200)
-      expect(@self_user.following?(@other_user)).to be(false)
-    end
-
-    it 'should return 400 and unfollow self failed', rpdoc_example_key: 400_200, rpdoc_example_name: 'unfollow self failed' do
-      @params[:following_id] = @self_user.id
-      post @path, params: @params.to_json, headers: @headers
-      expect(response).to have_http_status(400)
-      expect(json['error_code']).to eq(400_200)
-      expect(json['error_key']).to eq('invalid_following_target')
+      expect(@user.following?(following_user)).to be(false)
     end
   end
 end
